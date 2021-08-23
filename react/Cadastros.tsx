23eaac16bbdcc39949c695e4b094b57a041911de
mariaeduardaxs <45014607+mariaeduardaxs@users.noreply.github.com>
2021-08-23 02:30:48 -0300

    Update Cadastros.tsx

--- react/Cadastros.tsx
+++ react/Cadastros.tsx
@@ -15,11 +15,11 @@ const appformulario: StorefrontFunctionComponent<CadastrosProps> = ({}) => {
 
   return (
     <div className={styles.Container}>
-      <h1 className={styles.HeadingTitulo}>Cadastre-se para receber as novidades de UPMedal</h1>
+      <h1 className={styles.HeadingTitulo}>Cadastre-se para receber descontos e novidades da UPMedal!</h1>
       <form className={styles.Formulario} autoComplete="off">
         <input className={styles.inputForms} autoComplete="off" type="text" name="Nome" id="nome" value={nome} onChange={(e: any) => setNome(e.target.value)} required placeholder="Nome" />
         <input className={styles.inputForms} autoComplete="off" type="email" name="email" id="email" value={email} onChange={(e: any) => setEmail(e.target.value)} required placeholder="Email" />
-        <input className={styles.inputForms} autoComplete="off" type="text" name="celular" id="celular" value={celular} onChange={(e: any) => setCelular(e.target.value)} required placeholder="Telefone" />
+        <input className={styles.inputForms} autoComplete="off" type="text" name="celular" id="celular" value={telefone} onChange={(e: any) => setTelefone(e.target.value)} required placeholder="Telefone" />
         <input className={styles.inputButton} type="submit" value="Enviar" name='btnEnviar' id="btnEnviar" onClick={(e) => handleEnviar(e)} />
         <p className={styles.Mensagem}>{mensagem}</p>
       </form>
